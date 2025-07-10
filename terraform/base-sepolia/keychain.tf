bd7fec957e240d1da10d6d9c1ab2a190717eb3fd
Conner Swenberg <conner.swenberg@coinbase.com>
2025-07-10 16:08:50 -0700

    Purge terraform

--- terraform/base-sepolia/keychain.tf
+++
@@ -1,9 +0,0 @@
-resource "scm_keychain_allocation_config" "flywheel-protocol-base-sepolia" {
-  pool_id = "3dff1998-d258-4720-9676-055e144b3add"
-  name    = local.project_name
-}
-
-resource "scm_keychain_key" "flywheel-protocol-base-sepolia-base-sepolia-deploy-key" {
-  allocation_config_id = scm_keychain_allocation_config.flywheel-protocol-base-sepolia.id
-}
-
--- terraform/ethereum-sepolia/common.tf
+++
@@ -1,3 +0,0 @@
-locals {
-  project_name = "smart-contracts/contract-template" # UPDATE ME: This should be the same as the github repository name
-}
